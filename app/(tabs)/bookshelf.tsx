b2bf01da5d5302ce3d39422e637f177e2796b732
Carol Meng <meng1932@gmail.com>
2025-01-14 15:12:56 -0500

    add icons and adjust bookshelf page bg picture.

--- app/(tabs)/bookshelf.tsx
+++ app/(tabs)/bookshelf.tsx
@@ -1,25 +1,19 @@
-import { StyleSheet, Image, Platform, SafeAreaView } from 'react-native';
-
-import { Collapsible } from '@/components/common/Collapsible';
-import { ExternalLink } from '@/components/common/ExternalLink';
-import ParallaxScrollView from '@/components/common/ParallaxScrollView';
-import { ThemedText } from '@/components/ui/ThemedText';
-import { ThemedView } from '@/components/ui/ThemedView';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import Bookshelf from '@/components/Bookshelf';
+import { StyleSheet, Image, Platform, SafeAreaView } from "react-native";
+import ParallaxScrollView from "@/components/common/ParallaxScrollView";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import Bookshelf from "@/components/Bookshelf";
 
 export default function TabTwoScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
       headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
+        <Image
+          source={require("@/assets/images/bookshelf-bg.png")}
           style={styles.headerImage}
         />
-      }>
+      }
+    >
       <SafeAreaView style={styles.container}>
         <Bookshelf />
       </SafeAreaView>
@@ -29,13 +23,12 @@ export default function TabTwoScreen() {
 
 const styles = StyleSheet.create({
   headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
+    height: 250,
+    width: "100%",
+    position: "absolute",
   },
   titleContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     gap: 8,
   },
   container: {
