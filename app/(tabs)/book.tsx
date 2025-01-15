b93c2d4895e191c9cc88c45b782f5313e4cc62e3
Carol Meng <meng1932@gmail.com>
2025-01-15 16:29:22 -0500

    get the menu setted

--- app/(tabs)/book.tsx
+++ app/(tabs)/book.tsx
@@ -179,7 +179,6 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
   container: {
-    padding: 50,
     height:'100%'
   },
   headerImage: {
--- components/multi-step-form/StepWrapper.tsx
+++ components/multi-step-form/StepWrapper.tsx
@@ -47,7 +47,6 @@ const styles = StyleSheet.create({
   container: {
     justifyContent: "space-between",
     width: "100%",
-    height: "100%",
   },
   buttonText: {
     fontSize: 12,
