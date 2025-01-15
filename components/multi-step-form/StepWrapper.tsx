dc5e1f1ad0ba3f5ab80f38efb26daff683addc75
Carol Meng <meng1932@gmail.com>
2025-01-15 14:22:36 -0500

    rewrite the btn component

--- components/multi-step-form/StepWrapper.tsx
+++ components/multi-step-form/StepWrapper.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { View, Button, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Button, ButtonSpinner, ButtonText } from "../ui/button";
+import { colors } from "../ui/colors";
 
 interface StepWrapperProps {
   children: React.ReactNode;
@@ -22,13 +24,20 @@ export const StepWrapper: React.FC<StepWrapperProps> = ({
     <View style={styles.container}>
       <View style={styles.content}>{children}</View>
       <View style={styles.buttonContainer}>
-        <Button title="Cancel" onPress={onCancel} />
-        {!isFirstStep && <Button title="Previous Step" onPress={onPrevious} />}
-        {!isLastStep ? (
-          <Button title="Next Step" onPress={onNext} />
-        ) : (
-          <Button title="Submit" onPress={onNext} />
+        <Button className="p-3" onPress={onCancel}>
+          <ButtonText style={styles.buttonText}>Cancel</ButtonText>
+        </Button>
+
+        {!isFirstStep && (
+          <Button className="p-3" onPress={onPrevious}>
+            <ButtonText style={styles.buttonText}>Previous</ButtonText>
+          </Button>
         )}
+        <Button className="p-3" onPress={onNext}>
+          <ButtonText style={styles.buttonText}>
+            {isLastStep ? "Submit" : "Next"}
+          </ButtonText>
+        </Button>
       </View>
     </View>
   );
@@ -36,12 +45,18 @@ export const StepWrapper: React.FC<StepWrapperProps> = ({
 
 const styles = StyleSheet.create({
   container: {
-    /*    justifyContent: "space-between",
-    padding: 20, */
+    justifyContent: "space-between",
+    width: "100%",
+    height: "100%",
+  },
+  buttonText: {
+    fontSize: 12,
+    lineHeight: 12,
   },
   content: {},
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
+    width: "100%",
   },
 });
